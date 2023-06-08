ecd25bc26fa3daa878d9826dd1eb29524ba52fb1
ezequielbeider <eze-2001@hotmail.com>
2023-06-08 18:42:29 -0300

    hola

--- responsive-landing-page-headphones-main/assets/js/main.js
+++ responsive-landing-page-headphones-main/assets/js/main.js
@@ -76,6 +76,7 @@ const sr = ScrollReveal({
 sr.reveal(`.home__header, .section__title`,{delay: 600})
 sr.reveal(`.home__footer`,{delay: 700})
 sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
+sr.reveal(`.home__img2`,{delay: 900, origin: 'top'})
 
 sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
 sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
